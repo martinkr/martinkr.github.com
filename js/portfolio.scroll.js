dc67251b797e22384d031d20d726211bfb895bfd
martinkr <github@mkrause.info>
2013-11-08 17:57:48 +0100

    v2, beta

--- js/portfolio.scroll.js
+++ js/portfolio.scroll.js
@@ -1,103 +1,196 @@
+/*! Project: portfolio */
 /**
- * portfolioNAME JavaScript library:
+ * @author hello@mkrause.info
+ * @version 1.5
+ * @copyright hello@mkrause.info
  *
- * @portfolioDescription
- * @scroll
- *
- * @author $Author: martinkr $
- * @version $Revision: 0	$
- *
- * @author script@mkrause.info
- * @version 0.0
- * @copyright Neue Digitale / Razorfish
+ * Use scriptDoc comments - ever method requires a useful description
+ * Visit http://docs.aptana.com/docs/index.php/ScriptDoc_comprehensive_tag_reference
+ * for a help on the scriptDoc syntax
  *
  */
+
+
+/* jshint browser:true, jquery:true, strict: false, smarttabs:true, onevar:true */
+/* global jQuery:true, portfolio:true, -$:true */
+
 portfolio = {};
 
-portfolio.scroll = {
+/**
+ * @namespace
+ */
+portfolio.scroll = (function(){
 
-	__version: 0.0, // class version
-	__class: '', // class name
+	// private vars
+	var
+		_aSections = [],
+		_iThrottle = 50,
+ 		_bLockHandler=false,
+
+
 
 	/**
-	 * Adds class-specific events
+	 * channel handler
+	 * @param  {Object} sChannel_	normalized channel on which the message was published
+	 * @param  {Object} oData_		additional message data
 	 * @private
 	 * @return {Void}
-	 */
-	_addEvents: function() {
-		jQuery(window).on('scroll', portfolio.scroll.handler);
+	*/
+	_handler = function (sChannel_,oData_) {
+
+		switch (sChannel_) {
+
+			case '':
+
+			break;
+		}
+
+
 	},
 
 	/**
-	 * Prepares environment, usually called by constructor-function
+	 * Add section to the list
+	 * @param {Object} oData_ section details
+	 */
+	_addSection = function ($element_,sId_) {
+
+		var  _iPos;
+		if (!$element_) { return;}
+ 		// show as soon as the element is fully visible
+		_iPos = ($element_.size()) ? ( $element_.offset().top + $element_.outerHeight() ) * 0.9 : 0;
+		_aSections.push( {'show': _iPos, 'sId':sId_, '$element': $element_, 'data':$element_.data('detail') || {} });
+ 	},
+
+
+	/**
+	 * Actual scoll handler.
+	 * @return {Void}
+	 */
+	_doScroll = function () {
+		var _iScrollTop = jQuery(document).scrollTop(),
+			_iScrolled = _iScrollTop + jQuery(window).height(),
+			_i, _iLenght = _aSections.length
+		;
+
+		// cycle through all registered sections
+		for ( _i = 0; _i < _iLenght; _i++) {
+			// current viewport position (scroll + window height) shows the whole section
+
+			if(_iScrolled > _aSections[_i].show ) {
+					// call start()
+					console.log("call START() on" , _aSections[_i].sId);
+
+					_doAction(_aSections[_i]);
+
+					//remove
+					_aSections[_i] = null;
+				}
+		}
+
+		// purge array
+		_aSections = jQuery.grep(_aSections,function(n_){ return(n_); });
+
+		if(_iScrollTop > 15) {
+			jQuery('html').addClass('has-scrolled');
+		} else {
+			jQuery('html').removeClass('has-scrolled');
+		}
+
+		// remove scoll lock
+		_bLockHandler = false;
+	},
+
+	_doAction = function (oData_) {
+		console.log(arguments)
+		oData_.$element.addClass('fx-show');
+	},
+
+	/**
+	 * Scroll handler proxy
+	 * @param  {Event} event_ jQuery-event
+	 * @return {Void}
+	 */
+	_onScroll = function (event_) {
+ 		// currently running handler, ignore on ipad (scroll triggered after scrrolling is complete)
+		if (_bLockHandler === false /*|| _isNotIPad === false*/) {
+			_bLockHandler = true;
+			// call handler
+			window.setTimeout(portfolio.scroll.doScroll, _iThrottle);
+		}
+	},
+
+
+	/**
+	 * Constructor
+	 * @return  {Void}
 	 * @private
-	 * @return {Void}
 	 */
-	_setUp: function() {
-		portfolio.scroll._addEvents();
-	},
+	_initialize = function () {
+		var _i
+		;
+		_isNotIPad = !jQuery('html').hasClass('is-iPad');
 
-	/**
-	 * Contructor
-	 * @method initialize
-	 * @return {Void}
-	 */
-	initialize: function() {
-		var _scope = portfolio.scroll;
-		_scope._setUp();
-		jQuery('.cta').on('click',function () {jQuery('html, body').animate({'scrollTop':jQuery(window).height()+'px'}, 1000) });
-	},
-
-	getScrollY: function () {
-		return window.scrollY;
-	},
-
-	/**
-	 * @method handler
-	 * @param {Event}
-	 * @return {Void}
-	 */
-	handler: function(event_) {
-		var _iDiff = $('#projects').offset().top - portfolio.scroll.getScrollY(),
-			_$element = $('#sticky'),
-			_iEdge = $('header').position().top
-			;
-
-		if ( navigator.userAgent.toLowerCase().match(/ipad/) ) {
-			$('#projects').css('display','none')
-			// _$element.addClass('is-sticky');
-			// $('article:first').css('margin-top','100px');
-			// $('#projects').css('top','0')
-			return;
-		}
-
-		if (portfolio.scroll.getScrollY() <= 15 ) { jQuery('html').removeClass('is-moving'); }
-		if (portfolio.scroll.getScrollY() > 15 ) { jQuery('html').addClass('is-moving'); }
-		if (_iDiff < 1) {
-			_$element.css('position','fixed')
-		} else {_$element.css('position','absolute')};
- 		if (_iDiff <= _iEdge && _$element.css('position') !== 'fixed') {
-			_$element.addClass('is-sticky');
-			$('article:first').css('margin-top','100px');
-		}
-		if (_iDiff > _iEdge ) {
-
- 			_$element.removeClass('is-sticky');
- 			$('article:first').css('margin-top','50px');
-		}
-
-		jQuery('[data-layer]').each(function ( ){
-			return;
- 			if(jQuery(this).offset().top +jQuery(this).outerHeight() < jQuery(window).height() + jQuery(window).scrollTop() ) {
-				jQuery(this).find('span').stop().delay(250).animate({'opacity':0.75}).delay(250).animate({'opacity':0});
-			}
+		jQuery('[data-id] .fx-item').each(function(){
+			_addSection(jQuery(this),jQuery(this).data('id'));
 		})
-	}
-};
-// register onDocumentReady
-jQuery(document).ready(function () {
+
+		jQuery(window).on('scroll', portfolio.scroll.onEventScroll);
+		jQuery(window).on('resize', portfolio.scroll.onEventResize);
+	};
+
+	/**
+	 * Public API
+	 */
+	return {
+
+		/**
+		 * jQuery.Channel handler
+		 * @param  {Object} oData_		additional message data
+		 * @return {Void}
+		 */
+		onChannel: function (oData_) {
+			_handler(oData_.originalChannel,oData_);
+		},
+
+
+		/**
+		 * Returns application status object
+		 * @return {Object} status object
+		 */
+		/** @dev-*/
+		getStatus : function() {
+			return ;
+		},
+		/**-@dev*/
+
+		onEventScroll:_onScroll,
+		onEventResize:_onScroll,
+		doScroll:_doScroll,
+
+		/**
+		 * Constructor
+		 * @return {Void}
+		 */
+		initialize:_initialize
+	};
+
+})();
+
+// initialize
+jQuery(document).ready(function() {
 	portfolio.scroll.initialize();
-	portfolio.scroll.handler();
 });
+
+jQuery(window).load(function() {
+	jQuery('html').addClass('fx-intro-show');
+});
+
+
+
+
+
+
+
+
 
 
